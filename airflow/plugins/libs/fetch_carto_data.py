911da35cd631e91e13683494be08d9ce1dad8045
Aaron Couch <acinternets@gmail.com>
2023-01-02 00:01:28 -0500

    updates carto fetch

--- airflow/plugins/libs/fetch_carto_data.py
+++ airflow/plugins/libs/fetch_carto_data.py
@@ -2,29 +2,8 @@ import requests
 import json
 import sys
 
-carto_url = "https://phl.carto.com/api/v2/sql"
-table = "public_cases_fc"
-fields = [
-    "service_request_id",
-    "subject",
-    "status,status_notes",
-    "service_name",
-    "service_code",
-    "agency_responsible",
-    "service_notice",
-    "requested_datetime",
-    "updated_datetime",
-    "expected_datetime",
-    "closed_datetime",
-    "address",
-    "zipcode",
-    "media_url",
-    ]
-date_field = "requested_datetime"
-start_date = "2022-12-30"
-end_date = "2023-01-01"
-
-def fetch_carto_data(cartol_url, fields, table, where_clause):
+def fetch_carto_data_by_date(cartol_url, fields, table, date_field, start_date, end_date):
+    where_clause = f"{date_field} >= '{start_date}' AND {date_field} < '{end_date}'"
     string_fields = ','.join(fields)
     url = f"{cartol_url}?filename={table}&format=json&q=SELECT {string_fields} FROM {table} WHERE {where_clause}"
     response_API = requests.get(url)
@@ -34,9 +13,3 @@ def fetch_carto_data(cartol_url, fields, table, where_clause):
         print(f"Carto request returned with status: {response_API.status_code}")
         return sys.exit(1)
 
-def fetch_carto_data_by_date(date_field, start_date, end_date):
-    where_clause = f"{date_field} >= '{start_date}' AND {date_field} < '{end_date}'"
-    data = fetch_carto_data(carto_url, fields, table, where_clause)
-    return data
-
-fetch_carto_data_by_date(date_field, start_date, end_date)
