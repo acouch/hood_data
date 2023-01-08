4d79cd71cb956bb0e4209786c5edb2a3de80c265
Aaron Couch <acinternets@gmail.com>
2023-01-07 21:39:17 -0500

    Updates

--- airflow/plugins/libs/fetch_carto_data.py
+++ airflow/plugins/libs/fetch_carto_data.py
@@ -8,7 +8,7 @@ def fetch_carto_data_by_date(cartol_url, table, fields, date_field, start_date, 
     url = f"{cartol_url}?filename={table}&format=json&q=SELECT {string_fields} FROM {table} WHERE {where_clause}"
     response_API = requests.get(url)
     if response_API.status_code == 200:
-        return json.loads(response_API.text).rows
+        return json.loads(response_API.text)["rows"]
     else:
         print(f"Carto request to {url} returned with status: {response_API.status_code}")
         return sys.exit(1)
