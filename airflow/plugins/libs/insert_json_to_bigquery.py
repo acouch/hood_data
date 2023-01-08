48ee2154a250d4c22cf0059c64c651c015f8a96d
Aaron Couch <acinternets@gmail.com>
2023-01-07 21:54:46 -0500

    updates

--- airflow/plugins/libs/insert_json_to_bigquery.py
+++ airflow/plugins/libs/insert_json_to_bigquery.py
@@ -3,5 +3,8 @@ from google.cloud import bigquery
 def insert_json_to_bq(data, dataset, table):
     
     client = bigquery.Client()
-    insert = client.insert_rows_json(f'{dataset}.{table}', data)
+    dataset_ref = client.dataset(dataset)
+    table_ref = dataset_ref.table(table)
+    table = client.get_table(table_ref)
+    insert = client.insert_rows_json(table, data)
     return insert
