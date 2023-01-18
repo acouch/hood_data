8c44735206c56ff8c4ac2cd40f86db4f9b9d6a8a
Aaron Couch <acinternets@gmail.com>
2023-01-18 13:45:12 -0500

    Update fetch_carto_data.py

--- airflow/plugins/libs/fetch_carto_data.py
+++ airflow/plugins/libs/fetch_carto_data.py
@@ -2,10 +2,12 @@ import requests
 import json
 import sys
 
-def fetch_carto_data_by_date(cartol_url, table, fields, date_field, start_date, end_date):
+def build_cart_url(cartol_url, table, fields, date_field, start_date, end_date):
     string_fields = ','.join(fields)
     where_clause = f"{date_field} >= '{start_date}' AND {date_field} < '{end_date}'"
-    url = f"{cartol_url}?filename={table}&format=json&q=SELECT {string_fields} FROM {table} WHERE {where_clause}"
+    return f"{cartol_url}?filename={table}&format=json&q=SELECT {string_fields} FROM {table} WHERE {where_clause}"
+    
+def fetch_carto_data_by_date(url):
     response_API = requests.get(url)
     if response_API.status_code == 200:
         return json.loads(response_API.text)["rows"]
